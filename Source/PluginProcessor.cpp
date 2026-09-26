352288d05eaeabd2bbcec477f2dff726f66b26e6
snirgrapel420-dev <snirgrapel420@gmail.com>
2026-09-26 03:16:56 +0300

    Update PluginProcessor.cpp

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -303,10 +303,6 @@ juce::AudioProcessorEditor* DaliDistAudioProcessor::createEditor()
 {
     return new DaliDistAudioProcessorEditor (*this);
 }
-{
-    // Temporary: generic editor until the Dali Audio neon UI is built from the family screenshot
-    return new juce::GenericAudioProcessorEditor (*this);
-}
 
 void DaliDistAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
 {
